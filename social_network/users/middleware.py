46b68150225006bd5f0b2070bdbb6ff2f9a7ec97
Sasha <successamaz@gmail.com>
2021-05-05 22:28:16 +0300

    Social network

--- social_network/users/middleware.py
+++ social_network/users/middleware.py
@@ -6,6 +6,6 @@ from django.utils.deprecation import MiddlewareMixin
 class UserLastActivityMiddleware(MiddlewareMixin):
 	def process_view(self, request, view_func, view_args, view_kwargs):
 		if request.user.is_authenticated:
-			user_profile = Profile.objects.get(user__pk=request.user.pk)
+			user_profile = Profile.objects.get(user=request.user)
 			user_profile.last_activity = timezone.now()
 			user_profile.save()
